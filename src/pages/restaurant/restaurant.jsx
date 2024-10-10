dcb62672e4a7a78b59e66091af852fa72097800e
iam-mohammademam <abir141578@gmail.com>
2024-10-10 15:12:55 +0600

    initial commit

--- src/pages/restaurant/restaurant.jsx
+++ src/pages/restaurant/restaurant.jsx
@@ -11,7 +11,7 @@ const Restaurant = () => {
 
   const fetchData = async () => {
     try {
-      const res = await axios.get(`${import.meta.env.VITE_API_URL}/data`);
+      const res = await axios.get(`${import.meta.env.VITE_API_URL}/dish/list`);
       setData(res?.data);
     } catch (error) {
       console.log(error);
